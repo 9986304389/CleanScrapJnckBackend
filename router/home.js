76411ff3a69494e8c86765629f823f3d871a4e18
velpulakavitha <kavitha.v@medvol.in>
2024-02-23 19:25:29 +0530

    index

--- router/home.js
+++ router/home.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const user_login = require('../controllers/user_login');
 const router = express.Router();
 
+router.post('/usersiginup', user_login.usersiginup);
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
--- router/user_login_router.js
+++ router/user_login_router.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const user_login = require('../controllers/user_login');
+const user_login = require('../controllers/');
 const user_validation = require('../controllers/user_validation')
 const save_form_data = require('../controllers/study_form_data');
 const view_save_form_data = require('../controllers/view_study_form_data');
 const reset_password = require("../controllers/resetPassword");
 const router = express.Router();
 
-router.post('/usersiginup', user_login.usersiginup);
+router.post('/usersiginup', user_login.);
 router.post('/login', user_validation.authenticateUser);
 router.post('/save_form_data', save_form_data.study_form_data);
 router.post('/view_form_data', view_save_form_data.view_study_form_data);
