2066da4f70a33ee0baa268de0d8334310fdd89a5
velpulakavitha <kavitha.v@medvol.in>
2024-02-24 14:11:08 +0530

    test

--- router/routers.js
+++ router/routers.js
@@ -4,7 +4,7 @@ const user_validation = require('../controllers/user_validation')
 const router = express.Router();
 
 router.post('/usersiginup', user_login.usersiginup);
-router.post('/login', user_validation.authenticateUser);
+// router.post('/login', user_validation.authenticateUser);
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
