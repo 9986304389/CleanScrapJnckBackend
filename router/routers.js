054eb90d310284d3a8af5eddb98f455bfc30136e
velpulakavitha <kavitha.v@medvol.in>
2024-02-24 14:07:28 +0530

    token

--- router/routers.js
+++ router/routers.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const user_login = require('../controllers/user_login');
-const getToken = require('../controllers/getJSONJWTtoken');
 const user_validation = require('../controllers/user_validation')
 const router = express.Router();
 
 router.post('/usersiginup', user_login.usersiginup);
 router.post('/login', user_validation.authenticateUser);
-router.get('/gettoken', getToken.token);
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
