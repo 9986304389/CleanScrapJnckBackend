1f7a5e2762a1ccb302cda9b75ba981fc2a350ffe
velpulakavitha <kavitha.v@medvol.in>
2024-03-11 22:32:08 +0530

    user profile

--- controllers/user_login.js
+++ controllers/user_login.js
@@ -21,25 +21,25 @@ exports.usersiginup = async (req, res, next) => {
             return APIRes.getNotExistsResult(`Required ${inputs}`, res);
         }
 
-    
+
         let { name, email, phonenumber, password, shipping_address, billing_address, created_at } = userInput;
 
-          //Email and phonenumber check     
+        //Email and phonenumber check     
         if (!validateEmail(email) || !validatePhoneNumber(phonenumber)) {
             return APIRes.getFinalResponse(false, `Email or Phone Number is invalid`, [], res);
         }
 
-        const checkpassword=checkPasswordStrength(password)
+        const checkpassword = checkPasswordStrength(password)
         //Password check
-        if(checkpassword!=true){
-            return APIRes.getFinalResponse(false,checkpassword, [], res);
+        if (checkpassword != true) {
+            return APIRes.getFinalResponse(false, checkpassword, [], res);
         }
 
         client = await getClient();
 
         //email validation and phonenumber ------
         const existingRecordQuery = 'SELECT * FROM userdetails WHERE phonenumber = $1 or email =$2';
-        const existingRecordValues = [phonenumber,email];
+        const existingRecordValues = [phonenumber, email];
 
         const existingRecord = await client.query(existingRecordQuery, existingRecordValues);
 
@@ -102,75 +102,116 @@ function checkPasswordStrength(password) {
     return true;
 }
 
+exports.getprofile = async (req, res, next) => {
+    try {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            throw errors.array();
+        }
+
+        const userInput = Utils.getReqValues(req);
+        const requiredFields = ["email", "phonenumber"];
+        const inputs = validateUserInput.validateUserInput(userInput, requiredFields);
+        if (inputs !== true) {
+            return APIRes.getNotExistsResult(`Required ${inputs}`, res);
+        }
+
+
+        let { email, phonenumber } = userInput;
+
+        //Email and phonenumber check     
+        if (!validateEmail(email) || !validatePhoneNumber(phonenumber)) {
+            return APIRes.getFinalResponse(false, `Email or Phone Number is invalid`, [], res);
+        }
 
 
 
+        client = await getClient();
 
-// exports.editUserProfile = async (req, res, next) => {
-//     let client;
-//     try {
-//         let errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//             throw errors.array();
-//         }
+        //email validation and phonenumber ------
+        const existingRecordQuery = 'SELECT id,name,email,phonenumber,shipping_address,billing_address FROM userdetails WHERE phonenumber = $1 and email =$2';
+        const existingRecordValues = [phonenumber, email];
 
-//         const userInput = Utils.getReqValues(req);
-//         const requiredFields = ["phonenumber", "email"];
-//         const inputs = validateUserInput.validateUserInput(userInput, requiredFields);
-//         if (inputs !== true) {
-//             return APIRes.getNotExistsResult(`Required ${inputs}`, res);
-//         }
+        const existingRecord = await client.query(existingRecordQuery, existingRecordValues);
 
-//         let { phonenumber, email } = userInput;
+        if (existingRecord.rows.length != 0) {
 
-//         if (!validateEmail(email) || !validatePhoneNumber(phonenumber)) {
-//             return APIRes.getFinalResponse(false, `Email or Phone Number is invalid`, [], res);
-//         }
+            return APIRes.getFinalResponse(false, `Get user info successfully`, existingRecord.rows, res);
+        }
+        else {
+            return APIRes.getFinalResponse(false, `user not exists`, [], res);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return APIRes.getFinalResponse(false, `Internal Server Error`, [], res);
+    } finally {
+        // Close the client connection
+        if (client) {
+            await client.end();
+        }
+    }
+}
 
-//         client = await getClient();
+exports.editUserProfile = async (req, res, next) => {
+    let client;
+    try {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            throw errors.array();
+        }
 
-//         const existingRecordQuery = 'SELECT * FROM userdetails WHERE phonenumber = $1 and email=$2';
-//         const existingRecordValues = [phonenumber,email];
+        const userInput = Utils.getReqValues(req);
+        const requiredFields = ["phonenumber", "email"];
+        const inputs = validateUserInput.validateUserInput(userInput, requiredFields);
+        if (inputs !== true) {
+            return APIRes.getNotExistsResult(`Required ${inputs}`, res);
+        }
 
-//         const existingRecord = await client.query(existingRecordQuery, existingRecordValues);
+        let { name, email, phonenumber, password, shipping_address, billing_address, created_at } = userInput;
 
-//         if (existingRecord.rows.length != 0) {
-//             const updateQuery = `
-//             UPDATE userdetails 
-//             SET name = $1, 
-//             email=
-//                 shipping_address = $4, 
-//                 billing_address = $5,
-//                 created_at = $6 
-//             WHERE email = $7 AND phonenumber = $8 
-//             RETURNING *`;
-        
-//         const updateValues = [
-//             name,
-//             password,
-//             '0', // Assuming 'usertype' is updated to '0' in all cases
-//             shipping_address,
-//             billing_address,
-//             moment().tz('Asia/Calcutta').format('YYYY-MM-DD HH:mm:ss.SSS'),
-//             email,
-//             phonenumber
-//         ];
-        
-//         const result = await client.query(updateQuery, updateValues);
+        if (!validateEmail(email) || !validatePhoneNumber(phonenumber)) {
+            return APIRes.getFinalResponse(false, `Email or Phone Number is invalid`, [], res);
+        }
 
-//             if (result) {
-//                 return APIRes.getFinalResponse(true, `Profile created successfully.`, [], res);
-//             }
-//         } else {
-//             return APIRes.getFinalResponse(false, `user phone number already exists`, [], res);
-//         }
-//     } catch (error) {
-//         console.error('Error:', error);
-//         return APIRes.getFinalResponse(false, `Internal Server Error`, [], res);
-//     } finally {
-//         // Close the client connection
-//         if (client) {
-//             await client.end();
-//         }
-//     }
-// };
+        client = await getClient();
+
+        const existingRecordQuery = 'SELECT * FROM userdetails WHERE phonenumber = $1 and email=$2';
+        const existingRecordValues = [phonenumber, email];
+
+        const existingRecord = await client.query(existingRecordQuery, existingRecordValues);
+
+        if (existingRecord.rows.length != 0) {
+            const updateQuery = `
+            UPDATE userdetails 
+            SET name = $1, 
+            email=$2,
+            shipping_address = $3, 
+            billing_address = $4    
+            WHERE email = '${email}' AND phonenumber = '${phonenumber}'
+            RETURNING *`;
+
+            const updateValues = [
+                name,
+                email,
+                shipping_address,
+                billing_address
+            ];
+
+            const result = await client.query(updateQuery, updateValues);
+
+            if (result) {
+                return APIRes.getFinalResponse(true, `Edit profile successfully.`, [], res);
+            }
+        } else {
+            return APIRes.getFinalResponse(false, `user not exists`, [], res);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return APIRes.getFinalResponse(false, `Internal Server Error`, [], res);
+    } finally {
+        // Close the client connection
+        if (client) {
+            await client.end();
+        }
+    }
+};
--- router/routers.js
+++ router/routers.js
@@ -2,15 +2,11 @@ const express = require("express");
 const jwt = require('jsonwebtoken');
 const { secretKey } = require('../helperfun/jwtconfig');
 const user_login = require('../controllers/user_login');
-const getToken = require('../controllers/getJSONJWTtoken');
 const user_validation = require('../controllers/user_validation');
 const product = require('../controllers/Products/products')
 const router = express.Router();
 
-router.post('/usersiginup', user_login.usersiginup);
-router.post('/login', user_validation.authenticateUser);
-router.get('/getOTP', user_validation.otpGeneate);
-router.get('/verifyOTP', user_validation.verifyOTP);
+
 // Middleware function to check for JWT token in the Authorization header
 const checkToken = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
@@ -31,6 +27,9 @@ const checkToken = (req, res, next) => {
 };
 
 
+
+router.post('/usersiginup', user_login.usersiginup);
+router.post('/login', user_validation.authenticateUser);
 router.post('/addProduct', product.Addproducts);
 router.post('/Editproducts', product.Editproducts);
 router.post('/addProductstoCartByUser', product.addProductstoCartByUser);
@@ -40,10 +39,14 @@ router.post('/weBuyProductAddAndEdit', product.WeBuyProducts);
 router.post('/placeorder', product.placeorder)
 router.post('/updateOrderStatus', product.updateOrderStatus);
 router.post('/getOrdersByStatus', product.getOrdersByStatus);
+router.post('/editUserProfile', user_login.editUserProfile)
 
-router.get("/getAllProducts", checkToken,product.getallProducts);
+router.get('/getOTP', user_validation.otpGeneate);
+router.get('/verifyOTP', user_validation.verifyOTP);
+router.get('/getprofile', user_login.getprofile);
+router.get("/getAllProducts", checkToken, product.getallProducts);
 router.get("/getProductsByUser", product.getProductsByUser)
-router.get('/gettoken', getToken.token);
+
 router.get('/getAddressByUser', product.getAddressByUser);
 router.get('/getAllWeBuyProducts', product.getAllWeBuyProducts)
 
